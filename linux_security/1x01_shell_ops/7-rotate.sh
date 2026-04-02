c77ad77608ec9ecd31f685fb0d4f7a79129921c9
krapaud <mur.mickael@gmail.com>
2026-04-02 19:18:56 +0200

    Make 7-rotate.sh executable and ensure proper backup of large log files

---
+++ linux_security/1x01_shell_ops/7-rotate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ ! -d $1 ] ; then
+    exit 1
+fi
+mkdir -p $1/backups/
+for file in $1/*.log; do
+    size=$(stat -c%s "$file")
+    if [ $size -gt 1024 ]; then
+        gzip $file
+        mv "$file.gz" $1/backups/
+    else
+        echo "Skipping small file: $(basename $file)"
+    fi
+done
