2f116be5323af9d43e9803eec8dbea0a4b5466b3
krapaud <mur.mickael@gmail.com>
2026-04-03 14:14:55 +0200

    Refactor SSH hardening script to check for existing configurations before applying changes

--- linux_security/1x02_identity_management/3-harden_ssh.sh
+++ linux_security/1x02_identity_management/3-harden_ssh.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-sed -i 's/PermitRootLogin .*/PermitRootLogin no/' $1
-sed -i 's/PasswordAuthentication .*/PasswordAuthentication no/' $1
-sed -i 's/PubkeyAuthentication .*/PubkeyAuthentication yes/' $1
-sshd -t -f $1
+if grep -qE "^#?PermitRootLogin" $1; then
+    sed -i -E 's/^#?PermitRootLogin .*/PermitRootLogin no/' $1
+else
+    echo "PermitRootLogin no" >> $1
+fi
+if grep -qE "^#?PasswordAuthentication" $1; then
+    sed -i -E 's/^#?PasswordAuthentication .*/PasswordAuthentication no/' $1
+else
+    echo "PasswordAuthentication no" >> $1
+fi
+if grep -qE "^#?PubkeyAuthentication" $1; then
+    sed -i -E 's/^#?PubkeyAuthentication .*/PubkeyAuthentication yes/' $1
+else
+    echo "PubkeyAuthentication yes" >> $1
+fi
+if sshd -t -f $1; then
+    systemctl reload sshd
+fi
