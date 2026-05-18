b241e73acd302022006cb93d1293d0ef3259e384
krapaud <mur.mickael@gmail.com>
2026-05-18 15:47:50 +0200

    Fix formatting in CIDR to mask conversion script: combine binary calculation and output into a single line

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-binary="$(printf '%.0s1' $(seq 1 $1))$(printf '%.0s0' $(seq 1 $((32 - $1))))"
-printf "%d.%d.%d.%d\n" $(echo "ibase=2; ${binary:0:8}" | bc) $(echo "ibase=2; ${binary:8:8}" | bc) $(echo "ibase=2; ${binary:16:8}" | bc) $(echo "ibase=2; ${binary:24:8}" | bc)
+binary="$(printf '%.0s1' $(seq 1 $1))$(printf '%.0s0' $(seq 1 $((32 - $1))))";printf "%d.%d.%d.%d\n" $(echo "ibase=2; ${binary:0:8}" | bc) $(echo "ibase=2; ${binary:8:8}" | bc) $(echo "ibase=2; ${binary:16:8}" | bc) $(echo "ibase=2; ${binary:24:8}" | bc)
