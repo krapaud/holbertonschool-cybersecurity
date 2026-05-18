836302a1c3f7debf35a307faaa3108ad68dacbce
krapaud <mur.mickael@gmail.com>
2026-05-18 16:24:18 +0200

    Fix formatting in network ID calculation script: add newline at end of file

--- network_security/2x00_network_fundamentals/5-network_id.sh
+++ network_security/2x00_network_fundamentals/5-network_id.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-IFS='.' read -ra octets <<< "$1" ; IFS='.' read -ra mask <<< "$2"
-printf "%d.%d.%d.%d" $((${octets[0]} & ${mask[0]})) $((${octets[1]} & ${mask[1]})) $((${octets[2]} & ${mask[2]})) $((${octets[3]} & ${mask[3]}))
+IFS='.' read -ra octets <<< "$1" ; IFS='.' read -ra mask <<< "$2" ; printf "%d.%d.%d.%d" $((${octets[0]} & ${mask[0]})) $((${octets[1]} & ${mask[1]})) $((${octets[2]} & ${mask[2]})) $((${octets[3]} & ${mask[3]}))
