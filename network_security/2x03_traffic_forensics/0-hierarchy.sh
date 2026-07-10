fe11e57db1fab175a5f56570072894e928e71f3b
krapaud <mur.mickael@gmail.com>
2026-07-10 10:02:03 +0100

    Fix tshark command in hierarchy script to include -r and -q options for proper execution

--- network_security/2x03_traffic_forensics/0-hierarchy.sh
+++ network_security/2x03_traffic_forensics/0-hierarchy.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-# je demande à tshark les statistiques de hiérarchie des protocoles
-# tshark a un mode natif pour ça avec l'option -z
-# le fichier pcap c'est $1
-tshark -z io,phs $1
+tshark -z io,phs -r $1 -q
