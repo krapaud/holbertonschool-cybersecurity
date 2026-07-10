7f99b6fa6b7dd257d223c8f3f268565baf38f5bf
krapaud <mur.mickael@gmail.com>
2026-07-10 13:39:44 +0100

    Fix filter in password extraction script to correctly analyze URL-encoded forms

--- network_security/2x03_traffic_forensics/6-creds.sh
+++ network_security/2x03_traffic_forensics/6-creds.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tshark -r "$1" -Y "urlencoded-form.key" -T fields -e urlencoded-form.key -e urlencoded-form.value | awk '($1 == "password" || $1 == "pass" || $1 == "pwd") {print $2}'
+tshark -r "$1" -Y "urlencoded-form.value" -T fields -e urlencoded-form.key -e urlencoded-form.value | awk '($1 == "password" || $1 == "pass" || $1 == "pwd") {print $2}'
