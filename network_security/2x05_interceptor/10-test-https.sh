62a9b5437d2282b425b7a69eb63574601b1619bd
krapaud <mur.mickael@gmail.com>
2026-07-22 10:00:06 +0100

    Update expected HTTP response code for malware URL test script

--- network_security/2x05_interceptor/10-test-https.sh
+++ network_security/2x05_interceptor/10-test-https.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -x http://$1:3128 -o /dev/null -k -s -w "%{http_code}" https://malware.com # Expected: Connection refused or Error
+curl -x http://$1:3128 -o /dev/null -k -s -w "%{http_code}" https://malware.com # Expected: 403 Connection refused or Error
