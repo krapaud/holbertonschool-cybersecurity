0d10c613946bec8ee99d66c69f63444bd644ae0f
krapaud <mur.mickael@gmail.com>
2026-09-09 10:05:06 +0200

    Fix output formatting in 0-stats.sh to use explicit file paths

--- security_concepts/3x04_the_watchtower/0-stats.sh
+++ security_concepts/3x04_the_watchtower/0-stats.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 file="/var/log/auth.log"
-echo "File: ${file} - Lines: $(wc -l "${file}" | awk '{print $1}')"
+echo "File: /var/log/auth.log - Lines: $(wc -l /var/log/auth.log | awk '{print $1}')"
 
 file="/var/log/syslog"
-echo "File: ${file} - Lines: $(wc -l "${file}" | awk '{print $1}')"
+echo "File: /var/log/syslog - Lines: $(wc -l "${file}" | awk '{print $1}')"
 
 file="/var/log/kern.log"
-echo "File: ${file} - Lines: $(wc -l "${file}" | awk '{print $1}')"
+echo "File: /var/log/kern.log - Lines: $(wc -l "${file}" | awk '{print $1}')"
