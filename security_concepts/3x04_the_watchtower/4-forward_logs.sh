eac95faefd8dfff892cc50a9e0df2a6da9b4e633
krapaud <mur.mickael@gmail.com>
2026-09-14 10:46:22 +0200

    Fix log forwarding script to overwrite configuration file instead of appending

--- security_concepts/3x04_the_watchtower/4-forward_logs.sh
+++ security_concepts/3x04_the_watchtower/4-forward_logs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "*.* @127.0.0.1:514" | sudo tee -a /etc/rsyslog.d/50-default.conf
+echo "*.* @127.0.0.1:514" | sudo tee /etc/rsyslog.d/50-default.conf
 sudo systemctl restart rsyslog
 logger "Test Log Forwarding"
