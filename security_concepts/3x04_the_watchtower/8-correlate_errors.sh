ed57c0f500fd1c122d481fdc780e000001be1bcf
krapaud <mur.mickael@gmail.com>
2026-09-14 16:36:18 +0200

    Refactor error correlation script to improve alert logic and streamline processing

--- security_concepts/3x04_the_watchtower/8-correlate_errors.sh
+++ security_concepts/3x04_the_watchtower/8-correlate_errors.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+
 awk '{
     for (field = 2; field <= NF; field++) {
         if ($field ~ /^4[0-9][0-9]$/) {
-            errors[$1]++
+            print $1
             break
         }
     }
-}
-END {
-    for (ip in errors)
-        if (errors[ip] > 5)
-            print "ALERT: IP " ip " is scanning us!"
-}' "$1"
+}' "$1" | sort | uniq -c | while read -r count ip
+do
+    if [ "$count" -gt 5 ]
+    then
+        echo "ALERT: IP $ip is scanning us!"
+    fi
+done
