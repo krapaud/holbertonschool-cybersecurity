744c30eb8b4da37a09f9ee59d7c9a00c89452d8d
krapaud <mur.mickael@gmail.com>
2026-09-14 09:27:21 +0100

    Update 3-setup_loghost.sh to enable imudp and imtcp in rsyslog configuration

--- security_concepts/3x04_the_watchtower/3-setup_loghost.sh
+++ security_concepts/3x04_the_watchtower/3-setup_loghost.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-grep -nE 'imudp|imtcp|514' /etc/rsyslog.conf
+cp /etc/rsyslog.conf /etc/rsyslog.conf.bak
+
+sed -i \
+  -e 's/^#module(load="imudp")/module(load="imudp")/' \
+  -e 's/^#input(type="imudp" port="514")/input(type="imudp" port="514")/' \
+  -e 's/^#module(load="imtcp")/module(load="imtcp")/' \
+  -e 's/^#input(type="imtcp" port="514")/input(type="imtcp" port="514")/' \
+  /etc/rsyslog.conf
+
+systemctl restart rsyslog
