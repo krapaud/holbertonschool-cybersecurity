fc79786d1b6ee907626055965d51ea313f1267f1
krapaud <mur.mickael@gmail.com>
2026-09-14 15:27:52 +0200

    Update log rotation script to configure secure_remote.log settings

--- security_concepts/3x04_the_watchtower/6-setup_rotation.sh
+++ security_concepts/3x04_the_watchtower/6-setup_rotation.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "/var/log/secure_remote.sh" | sudo tee /etc/logrotate.d/secure_remote > /dev/null
+echo "/var/log/secure_remote.log {
+    daily
+    rotate 7
+    compress
+    missingok
+}" | sudo tee /etc/logrotate.d/secure_remote > /dev/null
